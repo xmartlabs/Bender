1c08fd7df43e72dee484f7147ea8a800d713f130
Diego Ernst <dernst@xmartlabs.com>
2017-07-21 18:44:55 -0300

    wip

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Bender
 
 class TestViewController: UIViewController {
 
-    let testRunner = BenderTestRunner()
-    
+//    let testRunner = BenderTestRunner()
+
+    var device: MTLDevice!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        testRunner.run()
+//        testRunner.run()
+        device = MTLCreateSystemDefaultDevice()
+
+        let url = Bundle.main.url(forResource: "test_concat", withExtension: "pb")!
+        let inputSize = LayerSize(f: 3, w: 3, h: 5)
+        let nnet = Network(device: device, inputSize: inputSize, parameterLoader: SingleBinaryLoader(checkpoint: "lala"))
+        let converter = TFConverter.default()
+        nnet.convert(converter: converter, url: url, type: .text)
+
+        nnet.initialize()
     }
 
 }
