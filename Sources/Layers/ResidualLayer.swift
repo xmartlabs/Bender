cc8c52b27ed78720825ba26909be56641c853be3
Santiago Castro <bryant@montevideo.com.uy>
2017-07-31 14:07:18 -0300

    Remove pointless "convSize" in ResidualLayer (#51)

--- Sources/Layers/ResidualLayer.swift
+++ Sources/Layers/ResidualLayer.swift
@@ -15,7 +15,7 @@ open class ResidualLayer: CompositeLayer {
     public var input: NetworkLayer
     public var output: NetworkLayer
     
-    public init(convSize: ConvSize, layers: Group, useBias: Bool = false, id: String? = nil) {
+    public init(layers: Group, useBias: Bool = false, id: String? = nil) {
         // We need a Dummy here because the Residual starts with a fork. When the network is initialized both of the layers on the fork will be connected to the residual's input. The dummy helps with pointer management.
         let resId = id ?? ""
         let residual = Dummy() // gets removed after graph is complete
