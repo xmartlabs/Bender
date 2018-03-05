d7229e4cd2b8f70e7055ddd86125612c819bb665
Mathias Claassen <mathias@xmartlabs.com>
2018-03-05 09:07:29 -0300

    Add header file

--- Sources/Metal/bender_metal_types.h
+++ Sources/Metal/bender_metal_types.h
@@ -4,9 +4,14 @@
 //
 //  Created by Mathias Claassen on 3/2/18.
 //
+using namespace metal;
 
 #ifndef bender_metal_types_h
 #define bender_metal_types_h
 
+typedef half texture_type;
+typedef half4 texture_type4;
+typedef half calculation_type;
+typedef half4 calculation_type4;
 
 #endif /* bender_metal_types_h */
