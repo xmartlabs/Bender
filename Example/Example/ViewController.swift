03a2eb3e96e4622bf7b45a374622e1ce2ca2db47
Mathias Claassen <mathias@xmartlabs.com>
2018-04-02 14:40:54 -0300

    Disable image recognition in iOS 10 (#101)

--- Example/Example/ViewController.swift
+++ Example/Example/ViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class TestViewController: UIViewController {
 
     let testRunner = BenderTestRunner()
+    @IBOutlet weak var imageRecognitionButton: UIButton!
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if #available(iOS 11, *) {
+            imageRecognitionButton.isEnabled = true
+        } else {
+            imageRecognitionButton.isEnabled = false
+            imageRecognitionButton.setTitle("Image recognition (iOS 11+ only)", for: .normal)
+        }
+    }
 
     @IBAction func run(_ sender: Any) {
         testRunner.run()
